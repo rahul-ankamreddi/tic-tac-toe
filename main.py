f824245a2c087f48f0c8ea633acdf395bbae2d8f
rahul-ankamreddi <rahul.ankamreddi@gmail.com>
2024-05-25 14:50:49 +0530

    main.py file with tic-tac-toe board template

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+if __name__=="__main__":
+    print("Welcome to Tic-Tac-Toe game:")
+    print(''' 
+         |     |
+      O  |  X  |  X  
+         |     |
+    `````|`````|`````
+      X  |  O  |  X
+         |     |
+    `````|`````|`````
+      X  |  O  |  O
+         |     | 
+ ''')
+    runs = True
+    while runs:
+        print("Give position : ")
+        runs = False
+    pass
