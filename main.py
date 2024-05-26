c910c5e2342b582a2dcfab6f945af1f8f9723273
rahul-ankamreddi <rahul.ankamreddi@gmail.com>
2024-05-26 12:29:10 +0530

    added positions for reference

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
+# tic-tac-toe game which can be played on terminal/console.
 
 if __name__=="__main__":
     print("Welcome to Tic-Tac-Toe game:")
     print(''' 
          |     |
-         |     |     
+         |     |      
          |     |
     `````|`````|`````
-         |     |   
+         |     |    
          |     |
     `````|`````|`````
-         |     |   
+         |     |    
          |     | 
+ ''',''' 
+      02    12    22   
+
+      01    11    21 
+
+      00    10    20 
  ''')
     
-    board = [['00',' '],['01',' '],['02',' '],['10',' '],['11',' '],['12',' '],['20',' '],['21',' '],['22',' ']]
+    board = [['00',' '],['10',' '],['20',' '],['01',' '],['11',' '],['21',' '],['02',' '],['12',' '],['22',' ']]
 
     runs = True
     turn = 0
 
     while runs:
-      #   print("Give position : ")
-        position = input("Give position : ")
+        if turn%2 == 0:
+            position = input("Player 'X' Give position : ")
+        else:
+            position = input("Player 'O' Give position : ")
+
+        if position == 'end':
+            break
 
         for move in board:
             if position == move[0]:
@@ -28,7 +40,8 @@ if __name__=="__main__":
                     move[1] = "X"
                 else:
                   move[1] = "O"
-                  
+                turn = turn + 1
+
         print(f''' 
          |     |
       {board[6][1]}  |  {board[7][1]}  |  {board[8][1]}  
@@ -39,6 +52,12 @@ if __name__=="__main__":
     `````|`````|`````
       {board[0][1]}  |  {board[1][1]}  |  {board[2][1]}
          |     | 
- ''')
-        runs = False
+ ''',''' 
+      02    12    22   
+
+      01    11    21 
+
+      00    10    20 
+ ''') 
+      #   runs = False
     pass
