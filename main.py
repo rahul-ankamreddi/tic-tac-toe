534296953f19372b4a528034c14a56e92916851f
rahul-ankamreddi <rahul.ankamreddi@gmail.com>
2024-05-30 12:13:53 +0530

    re-wrote the game using board as a matrix

--- main.py
+++ main.py
@@ -1,79 +1,125 @@
 # tic-tac-toe game which can be played on terminal/console.
 
-def check_winner(moves):
+import random
 
-    
-    
-    pass
 
-if __name__=="__main__":
-    print("Welcome to Tic-Tac-Toe game:")
+def display_board(board):
+    print(f'''
+             |     |
+          {board[0][0]}  |  {board[0][1]}  |  {board[0][2]}
+             |     |    
+        `````|`````|`````
+          {board[1][0]}  |  {board[1][1]}  |  {board[1][2]}
+             |     |    
+        `````|`````|`````
+          {board[2][0]}  |  {board[2][1]}  |  {board[2][2]}
+             |     |
+
+''')
+
+
+def select_symbol(symbols):
+    while True:
+        s = input(f"Choose a symbol from {[symbol for symbol in symbols]} : ")
+        if s in symbols:
+            break
+        else:
+            print("\n!!!  Invalid symbol  !!!\n")
+    symbols.remove(s)
+    return s
+
+
+def get_move(list_of_moves):
+    valid = False
+    while not valid:
+        move = input("Give valid move: ")
+        if move in list_of_moves:
+            valid = True
+            list_of_moves.remove(move)
+        else:
+            print("Give valid move")
+    return move
+
+
+def get_cpu_move(list_of_moves):
+    if len(list_of_moves) != 0:
+        move = random.choice(list_of_moves)
+        list_of_moves.remove(move)
+    else:
+        move = None
+    return move
+
+
+def check_winning_condition(board, player):
+    flag = True
+    for row in board:
+        if (row[0] == row[1] == row[2] == player):
+            flag = False
+    for i in range(3):
+        if (board[0][i] == board[1][i] == board[2][i] == player):
+            flag = False
+    if (board[0][0] == board[1][1] == board[2][2] == player):
+        flag = False
+    if (board[2][0] == board[1][1] == board[0][2] == player):
+        flag = False
+
+    if not flag:
+        print(f"\n***  Player {player} won!!  **\n")
+    return flag
+
+
+def make_move(board, player, move):
+    if move != None:
+        i, j = map(int, move)
+        board[i][j] = player
+    else:
+        print("\n*** No moves left ***\n")
+
+
+if __name__ == "__main__":
+
+    board = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+
+    list_of_moves = ['00', '01', '02', '10', '11', '12', '20', '21', '22']
+    list_of_symbols = ['x', 'o']
+
+    print("\n**  Welcome to tic-tac-toe game  **\n")
     print(''' 
-         |     |
-         |     |      
-         |     |
-    `````|`````|`````
-         |     |    
-         |     |
-    `````|`````|`````
-         |     |    
-         |     | 
- ''',''' 
-      02    12    22   
+    Rules:
+    ``````
+        1. Give position of the move as '01', where, 1st digit is row and 2nd is column
+        2. Top-left corner is '00'
+''')
 
-      01    11    21 
-
-      00    10    20 
- ''')
-    
-    board = [['00',' '],['10',' '],['20',' '],['01',' '],['11',' '],['21',' '],['02',' '],['12',' '],['22',' ']]
+    player_1 = select_symbol(list_of_symbols)
+    player_2 = list_of_symbols.pop()
 
     runs = True
     turn = 0
 
+    display_board(board)
+
     while runs:
-        if turn== 9:
-            print("Game ended!!!")
-            break
-
-        if turn%2 == 0:
-            position = input("Player 'X' Give position : ")
+        if turn % 2 == 0:
+            player = player_1
         else:
-            position = input("Player 'O' Give position : ")
+            player = player_2
 
-        if position == 'end':
-            break
+        if player == player_1:
+            move = get_move(list_of_moves)
+            make_move(board, player, move)
+            runs = check_winning_condition(board, player)
+            display_board(board)
 
-        for square in board:
-            if position == square[0]:
-                if square[1] == ' ':
-                    if turn%2 == 0:
-                        square[1] = "X"
-                    else:
-                        square[1] = "O"
-                    turn = turn + 1
-                else:
-                    print("\n** Position already filled give another **\n")
-                    break
-            
-                print(f''' 
-         |     |
-      {board[6][1]}  |  {board[7][1]}  |  {board[8][1]}  
-         |     |
-    `````|`````|`````
-      {board[3][1]}  |  {board[4][1]}  |  {board[5][1]}
-         |     |
-    `````|`````|`````
-      {board[0][1]}  |  {board[1][1]}  |  {board[2][1]}
-         |     | 
- ''',''' 
-      02    12    22   
+        if player == player_2:
+            print("CPU makes a move...")
+            move = get_cpu_move(list_of_moves)
+            make_move(board, player, move)
+            runs = check_winning_condition(board, player)
+            display_board(board)
 
-      01    11    21 
-
-      00    10    20 
- ''') 
-      
-        # runs = False
-
-    pass
+        turn = turn + 1
